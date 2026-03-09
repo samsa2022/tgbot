c7ee15b68f2b7b69773e4c520c7516b7ef8a6e7d
samsa2022 <samusenkokirill54@gmail.com>
2026-03-09 10:59:24 +0300

    ssss

--- main.py
+++ main.py
@@ -9,27 +9,28 @@ TOKEN = '8544193969:AAE70uodbN_N549epKi5_DDXYD8lCvDNIro'
 # Создаем экземпляр бота
 bot = telebot.TeleBot(TOKEN)
 
-# Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def start(message):
-   text = 'Hello'
-   # Отправляем сообщение text пользователю
-   bot.send_message(message.chat.id, text)
-
 @bot.message_handler(commands=['photo'])
 def send_photo(message):
     chat_id = message.chat.id
-    files = os.listdir(photo_directory)
-    if files:
-        random_file = random.choice(files)
-        file_path = os.path.join(photo_directory, random_file)
-        print(file_path)
+    if photo_directory:
+        file_path = random.choice(photo_directory)
         with open(file_path, 'rb') as photo_file:
             bot.send_photo(chat_id, photo_file)
     else:
         bot.send_message(chat_id, 'В данной директории нет файлов.')
 
-photo_directory = r"C:\Users\user\Desktop\dads"
+@bot.message_handler(commands=['add_meme'])
+def add_photo(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Отправьте свой мем.")
+
+
+
+
+
+photo_directory = ['photo/cat.png', 'photo/ff9a0ae6fbadcb9752710613a6e99f75.jpg', 'photo/mem.png']
+
+
 
 
 
